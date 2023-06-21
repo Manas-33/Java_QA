da0811c55543c90458c29138d24d3bb5af69a1cb
Manas Dalvi <dalvimanas33@gmail.com>
2023-06-22 00:50:35 +0530

    fix

--- src/f1/lecture2/DataTypes.java
+++ src/f1/lecture2/DataTypes.java
@@ -1,11 +1,14 @@
 package f1.lecture2;
 
+import java.util.*;
 public class DataTypes {
     public static void main(String[] args){
 
         int a;
         a = 10;
-
+        Scanner s = new Scanner(System.in);
+        char c = s.next().trim().charAt(0);
+        System.out.println(c);
 //        int a = 10;
 //        short s = 15;
 //        byte b = 11;
